ab7d530e0f99011b1e2bf4777301cb1a26784837
Lucky Luke <sz.pietrasz@wp.pl>
2025-01-12 14:59:55 +0100

    restrukturyzacja całego projektu do wersji stringa 2.6.1 i javy 17

--- src/main/java/bdbt_project/SpringApplication/MyErrorController.java
+++ src/main/java/bdbt_project/SpringApplication/MyErrorController.java
@@ -4,10 +4,8 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
-
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 @Controller
 public class MyErrorController implements ErrorController {
 
@@ -16,15 +14,19 @@ public class MyErrorController implements ErrorController {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-            if (statusCode == HttpStatus.FORBIDDEN.value()) {
+            if(statusCode == HttpStatus.FORBIDDEN.value()) {
                 return "errors/403";
-            } else if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            }
+            else if(statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "errors/404";
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 return "errors/500";
-            } else if (statusCode == HttpStatus.GATEWAY_TIMEOUT.value()) {
+            }
+            else if(statusCode == HttpStatus.GATEWAY_TIMEOUT.value()) {
                 return "errors/504";
-            } else {
+            }
+            else {
                 return "errors/other";
             }
         }
--- src/main/java/bdbt_project/SpringApplication/SecurityConfiguration.java
+++ src/main/java/bdbt_project/SpringApplication/SecurityConfiguration.java
@@ -2,29 +2,38 @@ package bdbt_project.SpringApplication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
-
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-
-                .requestMatchers("/", "/index").permitAll()
-                .requestMatchers("/resources/static/**").permitAll()
-                .requestMatchers("/error", "/errors/**").permitAll()
-                .requestMatchers("/main").authenticated()
-                .requestMatchers("/main_admin").access("hasRole('ADMIN')")
-                .requestMatchers("/main_user").access("hasRole('USER')")
-                .anyRequest().authenticated()
+    public PasswordEncoder getPasswordEncoder() { return NoOpPasswordEncoder.getInstance();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/resources/static/**").permitAll()
+                .antMatchers("/main").authenticated()
+                .antMatchers("/main_admin").access("hasRole('ADMIN')")
+                .antMatchers("/main_user").access("hasRole('USER')")
                 .and()
                 .formLogin()
                 .loginPage("/login")
@@ -32,27 +41,9 @@ public class SecurityConfiguration {
                 .permitAll()
                 .and()
                 .logout()
-                .logoutUrl("/logout") // Default logout URL
-                .logoutSuccessUrl("/index") // Redirect to index after logout
+                .logoutUrl("/index")
+                .logoutSuccessUrl("/index")
                 .permitAll();
-        return http.build();
-    }
 
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withUsername("user")
-                .password("user")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withUsername("admin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
