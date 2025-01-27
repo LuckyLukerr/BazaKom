dd260198e7ff879f4f788dd63e9881b1bb07a691
jkaliwod <01187337@pw.edu.pl>
2025-01-27 13:16:13 +0100

    fix

--- src/main/java/bdbt_project/SpringApplication/AppController.java
+++ src/main/java/bdbt_project/SpringApplication/AppController.java
@@ -22,6 +22,7 @@ public class AppController implements WebMvcConfigurer {
         registry.addViewController("/linie").setViewName("admin/linie");
         registry.addViewController("/pojazdy").setViewName("admin/pojazdy");
         registry.addViewController("/pasazerowie").setViewName("admin/pasazerowie");
+        registry.addViewController("/linie_user").setViewName("user/linie_user");
 
         registry.addViewController("/main_admin").setViewName("admin/main_admin");
         registry.addViewController("/main_user").setViewName("user/main_user");
@@ -30,7 +31,6 @@ public class AppController implements WebMvcConfigurer {
     //LINIE
     @Autowired
     private LinieDAO daoLinie;
-
     @RequestMapping("/linie")
     public String viewLinie(Model model) {
         List<Linie> listLinie = daoLinie.list();
@@ -47,11 +47,14 @@ public class AppController implements WebMvcConfigurer {
             return "redirect:/linie";
         }
     }
-    
+    @RequestMapping(value = "/updateLinie", method = RequestMethod.POST)
+    public String updateLinie(@ModelAttribute("linie") Linie linie) {
+        daoLinie.updateLinie(linie);
+        return "redirect:linie";
+    }
     @RequestMapping(value = "/editLinie", method = RequestMethod.POST)
     public String editLinie( @ModelAttribute("linie") Linie linie) {
         Linie existingLinie = daoLinie.get(linie.getId_lini());  // Pobierz istniejący rekord po ID
-
         // Zaktualizuj pola
         existingLinie.setId_lini(linie.getId_lini());
         existingLinie.setNazwa_linii(linie.getNazwa_linii());
@@ -61,17 +64,16 @@ public class AppController implements WebMvcConfigurer {
         existingLinie.setGodzina_zakonczenia(linie.getGodzina_zakonczenia());
         existingLinie.setId_centrali(linie.getId_centrali());
         existingLinie.setId_trasy(linie.getId_trasy());
-
         // Zaktualizuj rekord w bazie danych
         daoLinie.updateLinie(existingLinie);
         return "redirect:/linie"; // Przekierowanie do listy linii
     }
-
     @RequestMapping("/deleteLinie/{id_lini}")
     public String deleteLinie(@PathVariable(name = "id_lini") int id_lini) {
         daoLinie.deleteLinie(id_lini);
         return "redirect:/linie";
     }
+
 
     //PASAZEROWIE
     @Autowired
--- src/main/java/bdbt_project/SpringApplication/Linie.java
+++ src/main/java/bdbt_project/SpringApplication/Linie.java
@@ -1,5 +1,4 @@
 package bdbt_project.SpringApplication;
-
 public class Linie {
     private int id_lini;
     private String nazwa_linii;
@@ -9,10 +8,8 @@ public class Linie {
     private String godzina_zakonczenia;
     private String id_centrali;
     private String id_trasy;
-
     public Linie() {
     }
-
     public Linie(int id_lini, String nazwa_linii, String typ_linii, String dni_dzialania, String godzina_rozpoczecia, String godzina_zakonczenia, String id_centrali, String id_trasy) {
         this.id_lini = id_lini;
         this.nazwa_linii = nazwa_linii;
@@ -23,7 +20,6 @@ public class Linie {
         this.id_centrali = id_centrali;
         this.id_trasy = id_trasy;
     }
-
     @Override
     public String toString() {
         return "Linie{" +
@@ -37,67 +33,51 @@ public class Linie {
                 ", id_trasy='" + id_trasy + '\'' +
                 '}';
     }
-
     public int getId_lini() {
         return id_lini;
     }
-
     public void setId_lini(int id_lini) {
         this.id_lini = id_lini;
     }
-
     public String getNazwa_linii() {
         return nazwa_linii;
     }
-
     public void setNazwa_linii(String nazwa_linii) {
         this.nazwa_linii = nazwa_linii;
     }
-
     public String getTyp_linii() {
         return typ_linii;
     }
-
     public void setTyp_linii(String typ_linii) {
         this.typ_linii = typ_linii;
     }
-
     public String getDni_dzialania() {
         return dni_dzialania;
     }
-
     public void setDni_dzialania(String dni_dzialania) {
         this.dni_dzialania = dni_dzialania;
     }
-
     public String getGodzina_rozpoczecia() {
         return godzina_rozpoczecia;
     }
-
     public void setGodzina_rozpoczecia(String godzina_rozpoczecia) {
         this.godzina_rozpoczecia = godzina_rozpoczecia;
     }
-
     public String getGodzina_zakonczenia() {
         return godzina_zakonczenia;
     }
-
     public void setGodzina_zakonczenia(String godzina_zakonczenia) {
         this.godzina_zakonczenia = godzina_zakonczenia;
     }
-
     public String getId_centrali() {
         return id_centrali;
     }
-
     public void setId_centrali(String id_centrali) {
         this.id_centrali = id_centrali;
     }
-
     public String getId_trasy() {
         return id_trasy;
     }
-
     public void setId_trasy(String id_trasy) {
         this.id_trasy = id_trasy;
     }
